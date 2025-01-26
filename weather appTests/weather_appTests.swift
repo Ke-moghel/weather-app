db49a9fe09d40e43c63a3fd3619d09289ba9717c
boipelo matjila <bmatjila10@gmail.com>
2025-01-26 12:29:07 +0200

    Initial Commit

---
+++ weather appTests/weather_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  weather_appTests.swift
+//  weather appTests
+//
+//  Created by Kamogelo Matjila on 2025/01/26.
+//
+
+import Testing
+
+struct weather_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
